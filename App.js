b696e7835d589323fcb27bd04e2cc7cdde226d20
neves1697 <neves1697@gmail.com>
2023-12-10 15:18:53 -0300

    Refatorando pastas e arquivos

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Home from './src/Telas/Home';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Teste</Text>
+    <SafeAreaView style={styles.container}>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
---
+++ src/Telas/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Topo from './Components/Topo';
+
+export default function Home() {
+    return <>
+        <Topo />
+    </>
+}
