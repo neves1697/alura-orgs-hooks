8ca392bea35067d4301fd9a74a8b7f7c9dda39f7
neves1697 <neves1697@gmail.com>
2023-12-10 15:58:11 -0300

    Adicionando (Mocks) e ajustando alguns elementos de serviços para requisições

---
+++ src/Services/CarregaDados.js
@@ -0,0 +1,10 @@
+import produtores from "../Mocks/MockProdutores";
+import Topo from "../Telas/Home/Components/Topo";
+
+export const carregaProdutores = () =>{
+    return produtores;
+}
+
+export const carregaTopo = () =>{
+    return Topo;
+}
