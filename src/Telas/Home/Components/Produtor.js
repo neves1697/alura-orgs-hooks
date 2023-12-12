856305b43ba1e977bf827dee77acd65680c23124
neves1697 <neves1697@gmail.com>
2023-12-11 22:20:47 -0300

    Adicionando estilizações para o iOS e android

--- src/Telas/Home/Components/Produtor.js
+++ src/Telas/Home/Components/Produtor.js
@@ -1,20 +1,69 @@
 import React from "react";
-import { Text, View, Image } from 'react-native';
-
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
     return <>
-        <View>
-            <Image source={imagem}
+        <View style={estilos.cartao} >
+            <Image style={estilos.imagem} source={imagem}
                 accessibilityLabel={nome} // pessoas com deficiência visual possam ter acesso à informação.
             />
 
-            <View>
-                <Text> {nome} </Text>
-                <Text> {distancia} </Text>
+            <View style={estilos.informacoes} >
+                <Text style={estilos.nome} > {nome} </Text>
+                <Text style={estilos.distancia} > {distancia} </Text>
             </View>
-
-
         </View>
     </>
 }
+
+const estilos = StyleSheet.create({
+    cartao: {
+        backgroundColor: '#F6F6F6',
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 8,
+        flexDirection: 'row',
+        
+        //android
+        elevation: 4, //fazendo a sombra do cartão
+
+        // iOS
+        shadowColor: '#000',
+        textShadowOffset:{
+            width: 0,
+            height: 2
+        },
+
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62
+        
+    },
+
+    imagem: {
+        width: 48,
+        height: 48,
+        borderRadius: 8,
+        marginVertical: 16,
+        marginLeft: 16
+    },
+
+    informacoes: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        marginLeft: 8,
+        marginVertical: 16,
+        marginRight: 16
+    },
+
+    nome: {
+        fontSize: 14,
+        lineHeight: 22,
+        fontWeight: 'bold'
+    },
+
+    distancia: {
+        fontSize: 12,
+        lineHeight: 19,        
+    }
+});
