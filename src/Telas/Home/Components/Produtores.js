a6652d3390d8fcf6e7d42f520f82a1e75225b297
neves1697 <neves1697@gmail.com>
2023-12-11 21:30:33 -0300

    Adicionando (FlatList) ao projeto, exibindo a lista na tela inicial com o useState

--- src/Telas/Home/Components/Produtores.js
+++ src/Telas/Home/Components/Produtores.js
@@ -1,14 +1,28 @@
-import React, { useEffect } from "react";
-import { Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, Text } from 'react-native';
 import { carregaProdutores } from "../../../Services/CarregaDados";
 
 export default function Produtores() {
+    const [titulo, setTitulo] = useState('');
+    const [lista, setLista] = useState([]);
+
     useEffect(() => {
         const retorno = carregaProdutores();
-        console.log(retorno);
+
+        setTitulo(retorno.titulo);
+        setLista(retorno.lista);
+        //console.log(retorno);
     }, []);// os colchetes chaman apenas uma vez a função que está no useEffect()
 
     return <>
-        <Text>Teste</Text>
+        <FlatList
+            data={lista}
+            renderItem={({ item: { nome } }) =>
+                <Text> {nome} </Text>
+            }
+            ListHeaderComponent={() =>
+                <Text> {titulo} </Text>
+            }
+        />
     </>
 }
