11dbe1513fc531138a3794b168268fe2252f8895
neves1697 <neves1697@gmail.com>
2023-12-12 21:12:36 -0300

    Adicionando as estrelas e criando interatividade, onde é possível interagir com a quantidade, fazendo-a ficar ou não preenchida, em um máximo de 5 e um mínimo de 1

--- src/Components/Estrelas.js
+++ src/Components/Estrelas.js
@@ -1,21 +1,48 @@
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import estrela from '../../assets/estrela.png';
 import estrelaCinza from '../../assets/estrelaCinza.png';
 
 
 export default function Estrelas({
     quantidade: quantidadeAntiga,
-    editavel = false,
-    grande = false
+    editavel = true,
+    grande = true,
 }) {
 
+    const [quantidade, setQuantidade] = useState(quantidadeAntiga);
     const estilos = estilosFunction(grande);
+
+    const getImage = (index) =>{
+        if(index < quantidade){
+            return estrela;
+        }
+
+        return estrelaCinza;
+    }
+
+    const RenderEstrelas = () => {
+        const listaEstrelas = [];
+
+        for (let i = 0; i < 5; i++) {
+            listaEstrelas.push(
+                <TouchableOpacity
+                    key={i}
+                    onPress={() => { }}
+                    disabled={!editavel}
+                >
+                    <Image source={getImage(i)} style={estilos.estrela} />
+                </TouchableOpacity>
+            );
+
+        }
+
+        return listaEstrelas;
+    }
 
     return <>
         <View style={estilos.estrelas} >
-            <Image source={estrela} style={estilos.estrela} />
-            <Image source={estrela} style={estilos.estrela} />
+            <RenderEstrelas />
         </View>
     </>
 }
